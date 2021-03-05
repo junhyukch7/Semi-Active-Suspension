7ea8f7f2bee140b3fa723ad3d30891bb3867737e
junhyukch7 <pmp4569@naver.com>
2021-03-05 19:55:00 +0900

    first

---
+++ half_car_parameter.m
@@ -0,0 +1,17 @@
+clear 
+%half car parameter
+
+mc = 700; % sprung mass
+mf = 40; % front unsprung mass
+mr = 35.5; % rear unsprung mass
+lf = 2.3; % front axle to cog
+lr = 2.3; % cog to rear axle
+kfs = 19960; % front suspension spring rates
+krs = 17500; % rear suspension spring rates
+kft = 175500; % front tire spring rates
+krt = 175500; % rear tire spring rates
+cfs = 1290; % front suspension damping coefficient
+crs = 1620; % rear suspension damping coefficient
+crt = 1000; % rear tire damping constant
+cft = 1000; % front tire damping constant
+iyy = 2460; % inerita of body mass
